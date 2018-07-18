5d56fcacad6f8a62fccc990d2dc11a223289f41d
Andre Praeiro <andrepraeiro@gmail.com>
2018-07-18 16:04:07 -0400

    add server

---
+++ simpleCQRS/src/config/server.js
@@ -0,0 +1,32 @@
+const port  = 3005
+
+const bodyParser = require('body-parser')
+const express  = require('express')
+const server = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
+
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(allowCors)
+
+server.get('/', function(req, res){
+    res.send('Welcome to API')
+})
+
+// Lista de Utilizadores
+var users = [
+    { id: 1, username: 'Manuel', email: 'manuel@examplo.com' },
+    { id: 2, username: 'Maria', email: 'maria@examplo.com' }
+  ];
+
+
+server.get('/api/users/', function(req, res, next) {
+    res.send(users)
+})
+
+server.listen(port,function() {
+    console.log(`BACKEND  ir running on port ${port}.`)
+})
+
+module.exports = server
